cf238f581c9eb1f62a779c2eee9226f9f11cb00f
wichchasu <70121716+wichchasu@users.noreply.github.com>
2023-06-26 12:59:07 +0700

    first

---
+++ resources/js/Pages/Users/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    Hey {{ name }}!
+    <a :href="route('second')">Go to second page.</a>
+</template>
+<script>
+export default{
+    props:{
+        name: String,
+    },
+    data(){
+        return{};
+    },
+};
+</script>
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Users/Index', [
+        'name' => 'TestName',
+    ]);
+});
+
+Route::get('/second-link', fn()=>'Second link!')->name('second');
